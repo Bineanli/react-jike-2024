9500642573c744ad84f07278f9db93df81b32aad
Binean <binean.li@silksoftware.com>
2024-11-27 15:54:17 +0800

    封装token工具方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,11 @@
 //统一中转导出工具模块函数
 //import { request } from "@/utils";
 import { request } from "./request";
+import { getToken,setToken,removeToken } from "./token";
 
-export { request };
+export {
+     request,
+     getToken,
+     setToken,
+     removeToken
+    }
